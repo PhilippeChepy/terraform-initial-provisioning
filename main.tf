110b92abbfb009171e04cf4665a39b88754a9909
Philippe Chepy <philippe@chepy.eu>
2021-12-27 19:41:51 +0100

    fix: provisionning of large files (> 4MB)

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-resource "null_resource" "files" {
+resource "null_resource" "pre_exec_hooks" {
+  count = length(var.pre_exec) > 0 ? 1 : 0
+
   connection {
     host        = var.connection["host"]
     user        = var.connection["user"]
@@ -6,43 +8,61 @@ resource "null_resource" "files" {
   }
 
   triggers = {
-    target        = jsonencode(var.connection)
-    deploy_script = <<EOT
-#!/bin/bash
-set -euxo pipefail
+    target = jsonencode(var.connection)
+  }
 
-# Execute pre hooks
-%{for hook in var.pre_exec}
-${hook}
-%{endfor}
+  provisioner "remote-exec" {
+    inline = [for hook in var.pre_exec : "sudo ${hook}"]
+  }
+}
 
-# Provision files
-%{for target, file in var.files}
-# ${target}
-base64 -d << _EOF > ${target}
-${base64encode(file.content)}
-_EOF
-chown ${file.owner}:${file.group} ${target}
-chmod ${file.mode} ${target}
-%{endfor}
+resource "null_resource" "files" {
+  for_each   = var.files
+  depends_on = [null_resource.pre_exec_hooks]
 
-# Execute post hooks
-%{for hook in var.post_exec}
-${hook}
-%{endfor}
-EOT
+  connection {
+    host        = var.connection["host"]
+    user        = var.connection["user"]
+    private_key = var.connection["private_key"]
+  }
+
+  triggers = {
+    target    = jsonencode(var.connection)
+    filename  = each.key
+    file_hash = sha256(each.value.content)
   }
 
   provisioner "file" {
-    content     = self.triggers.deploy_script
-    destination = "/tmp/${sha256(self.triggers.deploy_script)}.sh"
+    content     = each.value.content
+    destination = "/tmp/${sha256(each.key)}"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/${sha256(self.triggers.deploy_script)}.sh",
-      "sudo /tmp/${sha256(self.triggers.deploy_script)}.sh",
-      "rm /tmp/${sha256(self.triggers.deploy_script)}.sh"
+      "sudo mv /tmp/${sha256(each.key)} ${each.key}",
+      "sudo chown ${each.value.owner}:${each.value.group} ${each.key}",
+      "sudo chmod ${each.value.mode} ${each.key}",
     ]
+  }
+}
+
+resource "null_resource" "post_exec_hooks" {
+  depends_on = [null_resource.files]
+
+  connection {
+    host        = var.connection["host"]
+    user        = var.connection["user"]
+    private_key = var.connection["private_key"]
+  }
+
+  triggers = {
+    target     = jsonencode(var.connection)
+    pre_hooks  = sha256(jsonencode(length(var.pre_exec) > 0 ? null_resource.pre_exec_hooks[0].triggers : {}))
+    files      = sha256(jsonencode({ for filename, specs in var.files : filename => null_resource.files[filename].triggers }))
+    post_hooks = jsonencode(var.post_exec)
+  }
+
+  provisioner "remote-exec" {
+    inline = [for hook in var.post_exec : "sudo ${hook}"]
   }
 }
